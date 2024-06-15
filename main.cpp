57de7db41246766545383952b59e22ec0bb71d2a
4хмед57 <4hm3d57@gmail.com>
2024-06-16 01:02:40 +0300

    Added drag and drop functionality for audio files

--- main.cpp
+++ main.cpp
@@ -1,97 +1,160 @@
 #include "include/raylib/raylib.h"
-#include <iostream>
+#include <cstring>
 
 #define WIDTH 800
 #define HEIGHT 600
 
+const char* PataFileName(const char* filePath) {
+	const char* fileName = strrchr(filePath, '/');
+	if (fileName) {
+		return fileName + 1; // Skip the '/'
+	}
+	fileName = strrchr(filePath, '\\');
+	if (fileName) {
+		return fileName + 1; // Skip the '\'
+	}
+	return filePath; // No path separators found, return the whole path
+}
+
+void progressBar(int barX, int barY, float progress, int remHr, int remMin, int remSec);
+void volumeBar(int barX, int barY, float volume, int vol);
+
 int main() {
-
-	InitWindow(WIDTH, HEIGHT, "music player");
+	InitWindow(WIDTH, HEIGHT, "test");
+	SetTargetFPS(60);
 	InitAudioDevice();
-	const char* filePath = "test.mp3";
-	Music music = LoadMusicStream(filePath);
-	PlayMusicStream(music);
 
+	const char* filePath = nullptr;
+	char fileName[512] = {0};
+	Music music;
+	bool musicLoaded = false;
+	bool isPause = false;
 	float volume = 0.5f;
-
-	SetMusicVolume(music, volume);
-
-	bool isPaused = false;
-
-
-	while(!WindowShouldClose()){
-		UpdateMusicStream(music);
-
-		if(IsKeyPressed(KEY_SPACE)){
-			if(IsMusicStreamPlaying(music)){
-				PauseMusicStream(music);
-				isPaused = true;
+	
+	while (!WindowShouldClose()) {
+		if (musicLoaded) {
+			UpdateMusicStream(music);
+		}
+		
+		// Drag and drop file to be played
+		if (IsFileDropped()) {
+			FilePathList droppedFiles = LoadDroppedFiles();
+			if (droppedFiles.count > 0) {
+				filePath = droppedFiles.paths[0];
+				strncpy(fileName, PataFileName(filePath), sizeof(fileName) - 1);
+				fileName[sizeof(fileName) - 1] = '\0';
+				if (musicLoaded) {
+					StopMusicStream(music);
+					UnloadMusicStream(music);
+				}
+				music = LoadMusicStream(filePath);
+				if(IsMusicReady(music)){
+					PlayMusicStream(music);
+					SetMusicVolume(music, volume);
+					musicLoaded = true;
+				} else {
+					TraceLog(LOG_WARNING, "Music file could not be loaded");
+					musicLoaded = false;
+				}
+				
 			}
-			else {
-				ResumeMusicStream(music);
-				isPaused = false;
-			}
+			UnloadDroppedFiles(droppedFiles);
 		}
-
-		if(IsKeyPressed(KEY_UP)){
-			volume += 0.1f;
-			if(volume > 1.0f) volume = 1.0f;
-			SetMusicVolume(music, volume);
+		
+		float timePlayed = 0.0f;
+		float fullTime = 0.0f;
+		float remTime = 0.0f;
+		int remHr = 0.0f;
+		int remMin = 0.0f;
+		int remSec = 0.0f;
+		float progress = 0.0f;
+		
+		if (musicLoaded) {
+			timePlayed = GetMusicTimePlayed(music);
+			fullTime = GetMusicTimeLength(music);
+			remTime = fullTime - timePlayed;
+			remHr = (int)remTime / 3600;
+			remMin = (int)(remTime / 60) % 60;
+			remSec = (int)remTime % 60;
+			progress = timePlayed / fullTime;
 		}
-
-		if(IsKeyPressed(KEY_DOWN)){ 
-			volume -= 0.1f;
-			if(volume < 0.0f) volume = 0.0f;
-			SetMusicVolume(music, volume);
-			
-		}
-
-		// progress bar variables
-		float timePlayed = GetMusicTimePlayed(music);
-		float full_time = GetMusicTimeLength(music);
-		float progress = timePlayed / full_time;
-		float remTime = (full_time - timePlayed) / 60;   // minutes
+		
+		// Progress bar variables
 		int barX = WIDTH - 50;
 		int barY = 20;
-
-
-		// volume variables
-		int volX = 10;
-		int volY = 350;
+		
+		// Volume bar variables
 		int volBarX = 20;
 		int volBarY = 100;
-		int volHeight = (int)(volBarY * volume);
+		int vol = volume * 100;
 		
+		if (IsKeyPressed(KEY_SPACE) && musicLoaded) {
+			if (IsMusicStreamPlaying(music)) {
+				PauseMusicStream(music);
+				isPause = true;
+			} else {
+				ResumeMusicStream(music);
+				isPause = false;
+			}
+		}
+		
+		if (IsKeyPressed(KEY_UP) && musicLoaded) {
+			volume += 0.1f;
+			if (volume > 1.0f) volume = 1.0f;
+			SetMusicVolume(music, volume);
+		}
+		
+		if (IsKeyPressed(KEY_DOWN) && musicLoaded) {
+			volume -= 0.1f;
+			if (volume < 0.0f) volume = 0.0f;
+			SetMusicVolume(music, volume);
+		}
 		
 		BeginDrawing();
-		
 		ClearBackground(GRAY);
 		
-		// progress bar
-		DrawText(TextFormat("File: %s", filePath), 10, 10, 20, BLACK);
-		DrawRectangle(10, 550, barX, barY, DARKGRAY);
-		DrawRectangle(10, 550, (int)(barX * progress), barY, RED);
-		DrawText(TextFormat("Time: %.2f", remTime), 10, 550, 20, BLACK);
-		
-		// voluem bar
-		// DrawRectangle(volBarX, volBarY - volBarMaxHeight, volX, volBarMaxHeight, DARKGRAY);
-		// DrawRectangle(volBarX, volBarY - volHeight, volX, volHeight, RED);
-
-		DrawRectangle(volX, volY - volBarY, volBarX, volBarY, DARKGRAY);
-		DrawRectangle(volX, volY - volHeight, volBarX, volHeight, RED);
-		
-		if(isPaused){
-			DrawText(TextFormat("Paused"), WIDTH /2 - MeasureText("Paused", 20), HEIGHT / 2, 20, BLACK);
+		if (filePath) {
+			DrawText(TextFormat("File: %s", fileName), 10, 10, 20, BLACK);
 		} else {
-			DrawText(TextFormat("Playing"), WIDTH /2 - MeasureText("Playing", 20), HEIGHT / 2, 20, BLACK);
+			DrawText("No file loaded.", 10, 10, 20, BLACK);
+		}
+		
+		if (musicLoaded) {
+			if (!isPause) {
+				DrawText("Playing", (WIDTH / 2) - (MeasureText("Playing", 20) / 2), HEIGHT / 2, 20, BLACK);
+			} else {
+				DrawText("Paused", (WIDTH / 2) - (MeasureText("Paused", 20) / 2), HEIGHT / 2, 20, BLACK);
+			}
+			
+			progressBar(barX, barY, progress, remHr, remMin, remSec);
+			volumeBar(volBarX, volBarY, volume, vol);
+		} else {
+			DrawText("Drag and drop file", (WIDTH / 2) - (MeasureText("Drag and drop file", 20) / 2), HEIGHT / 2, 20, BLACK);
 		}
 		
 		EndDrawing();
 	}
-
-	UnloadMusicStream(music);
+	
+	if (musicLoaded) {
+		StopMusicStream(music);
+		UnloadMusicStream(music);
+	}
+	
 	CloseAudioDevice();
 	CloseWindow();
 	return 0;
-	
+}
+
+void progressBar(int barX, int barY, float progress, int remHr, int remMin, int remSec) {
+	int musicProg = (int)(barX * progress);
+	DrawRectangle(10, 550, barX, barY, DARKGRAY);
+	DrawRectangle(10, 550, musicProg, barY, RED);
+	DrawText(TextFormat("Time: %02d:%02d:%02d", remHr, remMin, remSec), 10, 550, 20, BLACK);
+}
+
+void volumeBar(int barX, int barY, float volume, int vol) {
+	int volProg = (int)(barY * volume);
+	DrawRectangle(10, 350 - barY, barX, barY, DARKGRAY);
+	DrawRectangle(10, 350 - volProg, barX, volProg, RED);
+	DrawText(TextFormat("%d", vol), 10, 350, 15, BLACK);
 }
